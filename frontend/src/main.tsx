a4154b4bf2cadb373d25a421735cee8a2e888613
Azrael36999 <Aa18bday@gmail.com>
2026-01-31 18:38:29 -0700

    Create main.tsx

---
+++ frontend/src/main.tsx
@@ -0,0 +1,9 @@
+mport React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+<React.StrictMode>
+<App />
+</React.StrictMode>
+);
